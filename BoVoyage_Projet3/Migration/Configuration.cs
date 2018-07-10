5160344f0f506b25704a3baa09cceae7b6f8ddec
Nabil <abilaz24@gmail.com>
2018-07-10 11:14:21 +0200

    Ajout du dossier migration et de la class configuration

---
+++ BoVoyage_Projet3/Migration/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using BoVoyage_Projet3.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyage_Projet3.Migration
+{
+    public class Configuration : DbMigrationsConfiguration<BoVoyageDbContext>
+    {
+        //création d'un constructeur
+        public Configuration()
+        {
+            //migration non automatique
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
