d3c1ce21f87b1aac6c4e4a1bf7d6e18e495f48ba
Cyrille <Cyl-Dev@outlook.fr>
2018-07-10 11:13:36 +0200

    Mise à jour des relations entre classes metier

--- BoVoyage_Projet3/Model/AgenceVoyage.cs
+++ BoVoyage_Projet3/Model/AgenceVoyage.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyage_Projet3.Models
 {
+    [Table(name: "AgencesVoyage")]
     public class AgenceVoyage : BaseModel
     {
         public string Name { get; set; }
--- BoVoyage_Projet3/Model/Voyage.cs
+++ BoVoyage_Projet3/Model/Voyage.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyage_Projet3.Models
 {
+    [Table(name: "Voyages")]
     public class Voyage : BaseModel
     {
         public DateTime DateAller { get; set; }
@@ -15,8 +17,12 @@ namespace BoVoyage_Projet3.Models
 
         public decimal TarifToutCompris { get; set; }
 
+        [ForeignKey("IdAgenceVoyage")]
+        public AgenceVoyage AgenceVoyage { get; set; }
         public int IdAgenceVoyage { get; set; }
 
+        [ForeignKey("IdDestination")]
+        public Destination Destination { get; set; }
         public int IdDestination { get; set; }
     }
 }
